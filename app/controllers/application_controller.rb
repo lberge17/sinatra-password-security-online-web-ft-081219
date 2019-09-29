43d4a70a143255f2a153fcf82754a581cef8a11e
Laura Berge <lauraeberge@gmail.com>
2019-09-29 17:59:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,8 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		User.create(username: params["username"], password: params["password"])
+		user = User.create(username: params["username"], password: params["password"])
+		session[:user_id] = user.id
 	end
 
 	get "/login" do
